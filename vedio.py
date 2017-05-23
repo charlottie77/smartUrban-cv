331b9d776f864dd7d44f3d4e15c5a8a3078cf4ad
Unknown <charlottie77@163.com>
2017-05-23 14:35:44 +0800

    first
    vedio and draw line

---
+++ vedio.py
@@ -0,0 +1,18 @@
+import numpy as numpy
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+
+
+while(True):
+	ret, frame = cap.read()
+
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	img2 = cv2.line(gray,(0,0),(511,511),(0,0,0),5)
+	cv2.imshow('frame', img2)	
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+cap.release()
+cv2.destroyAllWindows()
